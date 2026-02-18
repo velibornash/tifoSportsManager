16211a16eac669c26a89cc51087913597484c8ce
mandzo-velibor <velja.jagodina@gmail.com>
2026-02-19 00:38:24 +0100

    Update PlayerMovementService.java

--- src/main/java/org/example/footballmanager/simulator/PlayerMovementService.java
+++ src/main/java/org/example/footballmanager/simulator/PlayerMovementService.java
@@ -5,6 +5,7 @@ import org.example.footballmanager.service.DemoMatchRuntime;
 import java.util.Comparator;
 import java.util.List;
 import java.util.Random;
+import org.springframework.stereotype.Component;
 
 @Component
 public class PlayerMovementService {
