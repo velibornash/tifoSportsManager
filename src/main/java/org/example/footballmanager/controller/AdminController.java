be8abc6ae70ebff074d45486ff7ff5adf81f4125
mandzo-velibor <velja.jagodina@gmail.com>
2026-02-22 05:11:56 +0100

    Update AdminController.java

--- src/main/java/org/example/footballmanager/controller/AdminController.java
+++ src/main/java/org/example/footballmanager/controller/AdminController.java
@@ -1,9 +1,12 @@
 package org.example.footballmanager.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.example.footballmanager.service.ResetService;
 import org.example.footballmanager.util.DatabaseInitializer;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/admin")
